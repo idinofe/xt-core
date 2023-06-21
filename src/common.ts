226ea658518033a6e33de6a30163549930a57ec8
JohnieXu <281910378@qq.com>
2023-06-21 08:35:28 +0800

    feat: 添加一些 web 相关的工具方法

--- src/common.ts
+++ src/common.ts
@@ -25,10 +25,30 @@ export const isString = (a: any): boolean => {
  * @param url 输入 URL
  * @returns 是否encoded过的 URI
  */
-export const isEncodeURILike = (url: string): Boolean => {
+export const isEncodeURILike = (url: string): boolean => {
   const reg = /%(\d)+/g
   const res = reg.test(url)
   return res
+}
+
+/**
+ * 校验字符串是否是 URL 链接
+ * @param url 任意字符串
+ * @returns 是否是 URL 链接
+ */
+export const isUrlLike = (url: string): boolean => {
+  const reg = /^http(s):\/\/?/
+  return reg.test(url)
+}
+
+/**
+ * 校验字符串是否是 URL.createObjectURL 返回的 blob 链接
+ * @param url 任意字符串
+ * @returns 是否是类似 blob 的链接
+ */
+export const isBlobUrlLike = (url: string): boolean => {
+  const reg = /^blob:/ // TODO:
+  return reg.test(url)
 }
 
 /**
--- src/web.ts
+++ src/web.ts
@@ -4,10 +4,17 @@
  * 1、复制文本到剪贴板
  */
 
+import { isBlobUrlLike, isUrlLike } from "./common"
+
 export const COPY_FAIL_MESSAGE = '当前环境不支持复制'
 
 type SObject = {
   [k: string]: string
+}
+
+interface IImageSize {
+  width: number
+  height: number
 }
 
 function createElement(tag: string, style: SObject) {
@@ -57,3 +64,68 @@ function createElement(tag: string, style: SObject) {
      document.body.removeChild(textArea)
    })
 }
+
+/**
+ * 文件对象转为 URL 链接（使用 URL.createObjectURL）
+ * @param {Blob | File} blob 文件对象
+ * @returns {{ url: string, revoke: Function }}
+ */
+export const convertBlobToUrl = (blob: Blob | File) => {
+  const url = URL.createObjectURL(blob)
+  function revoke() {
+    url && URL.revokeObjectURL(url)
+  }
+  return {
+    url,
+    revoke
+  }
+}
+
+/**
+ * 加载图片为 Image 对象
+ * @param {string} url 图片地址，完整地址、相对地址、 blob 地址或 base64 字符串
+ * @param {boolean} [isBase64 = false]  url 是否 base64 字符串
+ * @param {string} [baseUrl = ''] 图片基础路径
+ * @returns {HTMLImageElement} 图片 Image 对象
+ */
+export const loadImage = (url: string, isBase64: boolean = false, baseUrl: string = ''): Promise<HTMLImageElement> => {
+  return new Promise((resolve, reject) => {
+    let tempUrl = isBase64 ? url : (isUrlLike(url) || isBlobUrlLike(url)) ? url : baseUrl + url
+    const image = new Image()
+    image.src = tempUrl
+    image.crossOrigin = 'anonymous'
+    image.onload = () => { resolve(image) }
+    image.onerror = (e) => { reject(e) }
+  })
+}
+
+/**
+ * 获取图片尺寸
+ * @param {string | HTMLImageElement} data 图片地址或 Image 对象
+ * @param {boolean} [isBase64 = false] 是否 base64 字符串
+ * @param {string} [baseUrl = ''] 图片基础路径
+ * @returns {Promise<IImageSize>}
+ */
+function _getImageSize(data: string, isBase64: boolean, baseUrl: string): Promise<IImageSize>
+function _getImageSize(data: HTMLImageElement): Promise<IImageSize>
+// function _getImageSize(data: File): Promise<IImageSize>
+// TODO: 若使用函数重载的话，这里如何使用 typeof 区分 HTMLImageElement/File/string
+function _getImageSize(data: string | HTMLImageElement, isBase64: boolean = false, baseUrl: string = ''): Promise<IImageSize> {
+  if (typeof data === 'string') {
+    return new Promise((resolve, reject) => {
+      loadImage(data, isBase64, baseUrl).then((image) => {
+        resolve({
+          width: image.width,
+          height: image.height,
+        })
+      }).catch(reject)
+    })
+  } else {
+    return Promise.resolve({
+      width: data.width,
+      height: data.height,
+    })
+  }
+}
+
+export const getImageSize = _getImageSize
