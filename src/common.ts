b487b2a2512687f120a5673aa5230c103f5d6532
JohnieXu <281910378@qq.com>
2023-07-03 16:05:29 +0800

    feat: 添加修复乘除运算精度问题的方法floatMultiply floatDivide

--- src/common.ts
+++ src/common.ts
@@ -55,3 +55,100 @@ export const isBlobUrlLike = (url: string): boolean => {
  * 空函数
  */
 export const noop = () => {}
+
+/**
+ * 将科学计数法的数字转为字符串
+ * 说明：运算精度丢失方法中处理数字的时候，如果出现科学计数法，就会导致结果出错
+ * 4.496794759834739e-9  ==> 0.000000004496794759834739
+ * 4.496794759834739e+9  ==> 4496794759.834739
+ * @param {number} num 待转换值
+ */
+export const toNonExponential = (num: number): null | string => {
+  if (num == null) {
+    return num
+  }
+  if (typeof num !== 'number') {
+    return num
+  }
+  var m = num.toExponential().match(/\d(?:\.(\d*))?e([+-]\d+)/)
+  if (m == null) {
+    return null
+  }
+  return num.toFixed(Math.max(0, (m[1] || '').length - Number(m[2])))
+}
+
+/**
+ * 两浮点数相乘（修复了 js 乘法精度丢失问题）
+ * @param {number | string} arg1 浮点数 1
+ * @param {number | string} arg2 浮点数 2
+ */
+export const floatMultiply = (arg1: number | string, arg2: number | string): null | number => {
+  if(arg1 === null || arg2 === null) {
+    return null
+  }
+  arg1 = Number(arg1)
+  arg2 = Number(arg2)
+  if (Number.isNaN(arg1) || Number.isNaN(arg2)) {
+    return null
+  }
+  arg1 = toNonExponential(arg1) as any
+  arg2 = toNonExponential(arg2) as any
+  if (arg1 == null || arg2 == null) {
+    return null
+  }
+  var n1, n2
+  var r1, r2 // 小数位数
+  try {
+    r1 = arg1.toString().split('.')[1].length
+  } catch (e) {
+    r1 = 0
+  }
+  try {
+    r2 = arg2.toString().split('.')[1].length
+  } catch (e) {
+    r2 = 0
+  }
+  n1 = Number(arg1.toString().replace('.', ''))
+  n2 = Number(arg2.toString().replace('.', ''))
+  return n1 * n2 / Number(Math.pow(10, r1 + r2).toFixed(2))
+}
+
+/**
+ * 两浮点数相除（修复了 js 触发精度丢失问题）
+ * @param {string | number} arg1 被除数
+ * @param {string | number} arg2 除数
+ */
+export const floatDivide = (arg1: string | number, arg2: string | number): null | number => {
+  arg1 = Number(arg1)
+  arg2 = Number(arg2)
+  if (!arg2) {
+    return null
+  }
+  if (!arg1 && arg1 !== 0) {
+    return null
+  } else if (arg1 === 0) {
+    return 0
+  }
+  arg1 = toNonExponential(arg1) as any
+  arg2 = toNonExponential(arg2) as any
+  if (arg1 == null || arg2 == null) {
+    return null
+  }
+  var n1, n2
+  var r1, r2 // 小数位数
+  try {
+      r1 = arg1.toString().split('.')[1].length
+  } catch (e) {
+      r1 = 0
+  }
+  try {
+      r2 = arg2.toString().split('.')[1].length
+  } catch (e) {
+      r2 = 0
+  }
+  n1 = Number(arg1.toString().replace('.', ''))
+  n2 = Number(arg2.toString().replace('.', ''))
+  const result = floatMultiply((n1 / n2), Math.pow(10, r2 - r1))
+  return result
+  // return (n1 / n2) * Math.pow(10, r2 - r1);   // 直接乘法还是会出现精度问题
+}
