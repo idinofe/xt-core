cf7f72b63c2da203f647716b4734830e092b9c30
波妞 <50897761+meihuihui@users.noreply.github.com>
2023-08-14 16:35:38 +0800

    fix: 单元测试用例 (#33)
    * fix: 单元测试 #28 #30 #31
    
    * fix: 修复测试报错
    
    ---------
    
    Co-authored-by: JohnieXu <281910378@qq.com>
    Co-authored-by: mhh <meihuihui@whty.com.cn>

--- src/common.test.ts
+++ src/common.test.ts
@@ -1,5 +1,5 @@
 import Big from "big.js"
-import { delay, divide, floatDivide, floatMultiply, genMessageId, isEncodeURILike, isFormData, isFunction, isNormalObject, isNumber, isPromise, minus, multiply, plus, promisify, randomNumber, toNonExponential, isValidToken } from "./common"
+import { delay, divide, floatDivide, floatMultiply, genMessageId, isEncodeURILike, isFormData, isFunction, isNormalObject, isNumber, isPromise, minus, multiply, plus, promisify, randomNumber, toNonExponential, isValidToken, isString, isUrlLike, isBlobUrlLike } from "./common"
 
 describe.todo('isDef', () => {
 
@@ -30,6 +30,19 @@ describe('isNumber', () => {
   it('error case', () => {
     ['aa', '11', true, false, null, undefined, Symbol(), {}].forEach(i => {
       expect(isNumber(i)).toEqual(false)
+    })
+  })
+})
+
+describe('isString', () => {
+  it('normal case', () => {
+    ['aaa',''].forEach(i=>{
+      expect(isString(i)).toEqual(true)
+    })
+  })
+  it('error case', () => {
+    [true].forEach(i=>{
+      expect(isString(i)).toEqual(false)
     })
   })
 })
@@ -154,13 +167,13 @@ describe('randomNumber', () => {
     expect(() => randomNumber(Symbol() as any)).toThrowError('len must be an Number')
   })
 })
-
+// toThrowError 断言当调用函数时是否抛出错误。(必须将代码包装在一个函数中，否则错误将不会被捕获，测试将失败。)
 describe('genMessageId', () => {
   beforeEach(() => {
-    vi.useFakeTimers()
+    vi.useFakeTimers() // 启用模拟定时器（它将包装所有对计时器的进一步调用（例如Date）
   })
   afterEach(() => {
-    vi.useRealTimers()
+    vi.useRealTimers() // 返回使用前状态
   })
   it('normal case', () => {
     expect(() => genMessageId()).not.toThrowError()
@@ -168,7 +181,7 @@ describe('genMessageId', () => {
   })
   it('content matched to Date', () => {
     const date = new Date(2023, 6, 27)
-    vi.setSystemTime(date)
+    vi.setSystemTime(date) // 将当前日期设置为过去的日期
     expect(genMessageId().slice(0, 8)).toEqual('20230727')
     expect(genMessageId().slice(8, 16)).toEqual('00000000')
   })
@@ -537,6 +550,28 @@ describe('isNormalObject', () => {
   })
 })
 
+describe('isUrlLike', () => {
+  it('normal isUrlLike', () => {
+    expect(isUrlLike('http://?sdsfdg')).toEqual(true)
+    expect(isUrlLike('https://ssfsg.com')).toEqual(true)
+    expect(isUrlLike('https://')).toEqual(true)
+  })
+  it('not normal isUrlLike', () => {
+    expect(isUrlLike('1232')).toEqual(false)
+    expect(isUrlLike('')).toEqual(false)
+  })
+})
+
+describe('isBlobUrlLike', () => {
+  it('normal isBlobUrlLike', () => {
+    expect(isBlobUrlLike('blob:0100111')).toEqual(true)
+    expect(isBlobUrlLike('blob:787qwwwwwwwwwwwwww0100111')).toEqual(true)
+  })
+  it('not normal isBlobUrlLike', () => {
+    expect(isBlobUrlLike('wdwdwqa')).toEqual(false)
+  })
+})
+
 describe('isValidToken', () => {
    it('normal token', () => {
     expect(isValidToken('4a2886d21ff453bdf423c326d41913d1')).toEqual(true)
@@ -549,4 +584,4 @@ describe('isValidToken', () => {
      expect(isValidToken('  ')).toEqual(false)
      expect(isValidToken('[object Object]')).toEqual(false)
    })
-  })
+ })
