c1023b91cc8b44a88381c048e047ee7fb3c69414
JohnieXu <281910378@qq.com>
2023-09-13 16:32:55 +0800

    feat: web 模块添加判断当前环境下 indexedDB 是否可用方法: validateIndexedDBOpenable

---
+++ example/vue2/src/components/ValidateIndexedDBOpenable.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="validate-indexedDB-openable">
+    <p>是否支持indexedDB: {{ isIndexedDBSupport }}</p>
+  </div>
+</template>
+
+<script>
+import { validateIndexedDBOpenable } from '@dinofe/xt-core/web'
+
+export default {
+  name: 'ValidateIndexedDBOpenable',
+  data () {
+    return {
+      isIndexedDBSupport: '检测中'
+    }
+  },
+  mounted() {
+    validateIndexedDBOpenable.then(isSupport => {
+      this.isIndexedDBSupport = isSupport
+    })
+  }
+}
+</script>
